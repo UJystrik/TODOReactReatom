4a8c15364fbc3a8bd0886fdc0d43daef014f1dcd
UJystrik <61560254+UJystrik@users.noreply.github.com>
2022-11-17 20:48:02 +0300

    correction of the checkbox logic

--- my-app/src/features/todoitem/todoItem.tsx
+++ my-app/src/features/todoitem/todoItem.tsx
@@ -1,17 +1,20 @@
 import { ITodoItem } from "../types";
 import { useAction } from "@reatom/react";
 import { checkTodo, deleteTodo } from "../actions";
-import { useState } from "react";
 import  "./todoitem.css";
 
 export default function TodoItem(item: ITodoItem) {
-    const [checked, setChecked] = useState(item.complete);
     const deleteItem = useAction(deleteTodo);
     const checkItem = useAction(checkTodo);
 
     return( 
     <div className={"todoCase"}>
-        <input className={"todoItemImg"} type="checkbox" checked={checked} onChange={() => {setChecked(!checked); checkItem(item.id)}}/>
+        <input 
+            className={"todoItemImg"} 
+            type="checkbox" 
+            checked={item.complete} 
+            onChange={() => {checkItem(item.id)}}/>
+
         <div className={item.complete? "checkText" : ""}>
             {item.text}
         </div>
